8ca18d8032b968ae0295cb33f6e9db8ff83e45f7
Noah Feder <noah.feder@gmail.com>
2016-07-20 20:41:48 -0400

    finish work for the night

--- main.js
+++ main.js
@@ -2,20 +2,52 @@
 $(function(){
     var $guybrush = $('.guybrush');
 
-    //canPickUp, canUseWithWhat?, canPlaceWhere?, canOpen[, canPush/canPull]
+    //name, className, width, height, top, left required for positioning; others are handled by prototypes
 
-    function Item(name, image, storable, openable, usable, items) {
+    function Item(name, className, top, left, storable, openable, usable, container, items) {
       this.name = (name) ? String(name) : 'missingNo';
-      this.image = (image) ? String(image) : 'img/missingNo.png';
-      this.storable = (typeof(storable) === 'boolean') ? storable : false;
-      this.openable = (typeof(openable) === 'boolean') ? openable : false;
-      this.usable = (typeof(usable) === 'boolean') ? usable : false;
-      this.items = (typeof(items) === 'object') ? items : [];
+      this.className = (className) ? String(className) : 'img/missingNo.png';
+      this.top = String(top) + 'px';
+      this.left = String(left)+ 'px';
+      this.storable = storable;
+      this.openable = openable;
+      this.usable = usable;
+      this.console = container;
+      this.items = items;
     }
 
-    var chest = new Item();
-    console.log(chest);
-    //animates guybrush walking.
+    Item.prototype.storable = false;
+    Item.prototype.openable = false;
+    Item.prototype.usable = false;
+    Item.prototype.container = false;
+    Item.prototype.items =  [];
+    Item.prototype.place = function() {
+      var $item = $('<div>');
+      $item.addClass(this.className + ' item');
+      $item.css({
+        top: this.top,
+        left: this.left
+      });
+      $item.appendTo('.window');
+    };
+
+
+
+    var chest = new Item('chest','chest',300,100,false,true,false,true,['bugs']);
+    chest.place();
+
+    var chest2 = new Item('chest2','chest open',300,200,false,true,false,true,['rats']);
+    chest2.place();
+
+    var chest3 = new Item('chest3','chest',300,300,false,true,false,true,['keys']);
+    chest3.place();
+
+    var gate = new Item('gate','gate',380,600);
+    gate.place();
+
+    var turtle = new Item('turtle','turtle',359,476);
+    turtle.place();
+    // Guybrush object! Contains statuses and methods
 
     var guybrush = {
       'alive': true,
@@ -36,15 +68,64 @@ $(function(){
         if (this.right) {
           $guybrush.addClass('right');
         }
-      }
+      },
+      'inventory':{}
     }
 
+    var input = {
+      'parse' : function(input) {
+      var inputArr = input.toLowerCase().split(' ');
+      switch (inputArr[0]) {
+        case 'right':
+          guybrush.stop();
+          $guybrush.removeClass('right');
+          guybrush.walk();
+          setTimeout(guybrush.stop,1200);
+          var curr = parseInt($guybrush.css('left'));
+          curr = (curr >= 715) ? 750 : curr + 35;
+          $guybrush.css('left', curr+'px');
+          break;
+        case 'left':
+          guybrush.stop();
+          $guybrush.addClass('right');
+          guybrush.walk();
+          setTimeout(guybrush.stop,1200);
+          var curr = parseInt($guybrush.css('left'));
+          curr -= (curr <= 35) ? curr : 35;
+          $guybrush.css('left', curr+'px');
+          break;
+        case 'stop':
+          guybrush.stop();
+          break;
+        //TODO ADD VERB ACTIONS
 
-    $('.walk').on('click',guybrush.walk);
-    $('.stop').on('click',guybrush.stop);
-    $('.rightbutt').on('click',function(){
-      $guybrush.toggleClass('right');
+        default: this.error();
+      }
+      },
+      'print' : function(msg) {
+        if (msg) {
+          //TODO  ADD ::BEFORE TO GUYBRUSH
+        }
+      },
+      'capitalize' : function(word) {
+        return word.charAt(0).toUpperCase() + word.substring(1);
+      },
+      'init': function() {
+        $('input').on('keyup',function(e){
+        if (e.keyCode === 13) {
+          var $inputField = $('input');
+          input.parse($inputField.val());
+          $inputField.val('');
+        }
+        })
+      },
+      'error': function() {
+        this.print('I didn\'t quite get that, pal.');
+      }
+    }
+    input.init();
+    $('.kill').click(function(){
+      $('.item').remove();
     })
-
 
 });
